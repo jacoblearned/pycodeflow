28b4ffc148db04f6bf55914979f78ab26dd809a3
jacoblearned <jacob@scientismist.com>
2018-08-05 19:27:29 -0400

    Add invoke namespace for top-level and unit-test

--- tasks.py
+++ tasks.py
@@ -1,12 +1,19 @@
 """ Project tasks """
-from invoke import task
+from invoke import task, Collection
+from invoke.tasks import call
+
+NS = Collection()
 
 
 @task
-def setup(ctx, version="3"):
+def setup(ctx, version="3", skip_lock=False):
     """ Create pipenv with target python version and install dependencies """
+    skip_lock = "--skip-lock" if skip_lock else ""
     ctx.run("pipenv --python {}".format(version))
-    ctx.run("pipenv install --dev")
+    ctx.run("pipenv install {} --dev".format(skip_lock))
+
+
+NS.add_task(setup)
 
 
 @task
@@ -15,16 +22,34 @@ def fmt(ctx):
     ctx.run("pipenv run black pycodeflow tests tasks.py setup.py")
 
 
+NS.add_task(fmt)
+
+
 @task
 def lint(ctx):
     """ Lint code """
     ctx.run("pipenv run pylint pycodeflow tasks.py setup.py")
 
 
-@task
-def unit_test(ctx):
-    """ Run unit tests """
+NS.add_task(lint)
+
+
+@task(pre=[call(setup, version="2.7", skip_lock=True)])
+def unit_test_two(ctx):
+    """ Run unit tests in a 2.7 environment """
     ctx.run("pipenv run pytest --cov-report term-missing --cov=pycodeflow tests/")
+
+
+@task(setup)
+def unit_test_three(ctx):
+    """ Run unit tests in a Python 3.x environment """
+    ctx.run("pipenv run pytest --cov-report term-missing --cov=pycodeflow tests/")
+
+
+UT = Collection("unit_test")
+UT.add_task(unit_test_two, "two")
+UT.add_task(unit_test_three, "three", default=True)
+NS.add_collection(UT)
 
 
 @task
@@ -33,10 +58,16 @@ def clean(ctx):
     ctx.run("rm -rf dist/")
 
 
+NS.add_task(clean)
+
+
 @task(clean)
 def build(ctx):
     """ Build dist """
     ctx.run("pipenv run python setup.py sdist")
+
+
+NS.add_task(build)
 
 
 @task(build)
@@ -45,7 +76,13 @@ def release(ctx):
     ctx.run("pipenv run twine upload dist/*")
 
 
+NS.add_task(release)
+
+
 @task
 def install_hooks(ctx):
     """ Install Pre-Commit pre-push hooks """
     ctx.run("pipenv run pre-commit install --hook-type pre-push")
+
+
+NS.add_task(install_hooks)
