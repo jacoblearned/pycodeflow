67d56bc1f48290987b7928a9b28371ad3e4abcfa
jacoblearned <jacob@scientismist.com>
2018-08-04 20:56:40 -0400

    Add project dir and info

---
+++ pycodeflow/__init__.py
@@ -0,0 +1,9 @@
+""" Pycodeflow: A tool to generate PlantUML sequence diagrams from code execution """
+
+NAME = "pycodeflow"
+VERSION = "0.0.1"
+DESC = __doc__
+AUTHOR = "Jacob Learned"
+AUTHOR_EMAIL = "jacobmlearned@gmail.com"
+URL = "https://github.com/jacoblearned/pycodeflow"
+LICENSE = "MIT"
